70d85130d45b40dd0cf8df1465131ae98a13dfff
Grzegorz <gb87@wp.pl>
2015-10-13 21:20:58 +0200

    Add_routes_to_routes_file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+  root :to => 'devise/sessions#new'
+  end
 
   resources :students do
     get 'subjects'
   end
-  get 'subjects' => 'reports#subjects'
+  get 'report_subjects' => 'reports#subjects'
   get 'index' => 'visitors#index'
   resources :teachers
+  
 end
