929a9610cd6c3a7d6ec3cf56f758e2b48eba6cb7
Grzegorz <gb87@wp.pl>
2015-10-13 22:51:56 +0200

    Add_decent_decorator_gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do
-  root :to => 'devise/sessions#new'
-  end
+  
+  root :to => 'visitors#index'
 
   resources :students do
     get 'subjects'
