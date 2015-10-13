41d15289056bb10800551f7e6ad0372a728d3a3f
Grzegorz <gb87@wp.pl>
2015-10-13 20:07:22 +0200

    Repair_teacher_decorator

--- app/decorators/teacher_decorator.rb
+++ app/decorators/teacher_decorator.rb
@@ -3,4 +3,9 @@ class TeacherDecorator < BaseDecorator
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def full_name_with_title
+  	full_name+" [#{model.academic_title}]"
+  end
+
 end
